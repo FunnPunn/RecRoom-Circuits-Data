f11ab0c1b1bbf911ab237b956d81e92b5cd86be4
FunnPunn <rrobuxian@gmail.com>
2023-03-01 22:03:18 +0100

    change port groups

--- index.js
+++ index.js
@@ -9,47 +9,48 @@ var Out = {}
 
 var key = Keys[0]
 
-let ShouldWrite = false
+let ShouldWrite = true
 
 var dict = {}
 
 if(ShouldWrite) {
     for(var k of Keys){
+      if(k["NodeDescs"][0] === undefined) {console.error(k); break;}
+
+      console.log(k["NodeDescs"][0])
+      var Ports = k["NodeDescs"][0]
+      let ReadonlyTypeParams = Ports["ReadonlyTypeParams"]
+      if(ReadonlyTypeParams[0] !== null){
+          for(var [key, declar] of Object.entries(ReadonlyTypeParams)){
+              var newtypes = declar.replace("(", "").replace(")", "").split(" ").filter((str) => str !== '' && str !== '|')
+              dict[key]=newtypes
+          }
+          for(var arr of Ports["Inputs"]){
+            let type = arr["ReadonlyType"]
+            if (type in dict){
+              arr["ReadonlyType"] = dict[type]
+            }
+          }
+          for(var arr of Ports["Outputs"]){
+            let type = arr["ReadonlyType"]
+            if (type in dict){
+              arr["ReadonlyType"] = dict[type]
+            }
+          }
+      }  
+
         Out[k["ReadonlyChipName"]] = {
             "Description": k["Description"],
             "IsBeta": k["IsBetaChip"],
             "IsTrollingRisk": k["IsTrollingRisk"],
             "DeprecationStage": k["DeprecationStage"],
-            "NodeDescs": k["NodeDescs"]
+            "Inputs": k["NodeDescs"][0]["Inputs"],
+            "Outputs": k["NodeDescs"][0]["Outputs"]
         }
     }
     fs.writeFileSync("chips/chips.json", JSON.stringify(Out, null, 4))
 }
-
-let PortsToChange = {
-    "NodeDescs": [
-        {
-          "Name": "Absolute Value",
-          "ReadonlyTypeParams": {
-            "T": "(float | int | vector3)"
-          },
-          "Inputs": [
-            {
-              "Name": "Value",
-              "ReadonlyType": "T",
-              "Description": ""
-            }
-          ],
-          "Outputs": [
-            {
-              "Name": "Result",
-              "ReadonlyType": "T",
-              "Description": ""
-            }
-          ]
-        }
-      ]
-}
+/*
 var Ports = PortsToChange["NodeDescs"][0]
 let ReadonlyTypeParams = Ports["ReadonlyTypeParams"]
 if(ReadonlyTypeParams[0] !== null){
@@ -70,7 +71,8 @@ if(ReadonlyTypeParams[0] !== null){
       }
     }
     console.log(Ports)
-} else console.log("no");
+} else console.log("No valid TypeParams");
+*/
 
 /* JSON Structure:
 {
