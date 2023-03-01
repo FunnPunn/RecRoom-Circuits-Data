524e96517feb250e339a1d91765726d20fb29853
FunnPunn <84284151+FunnPunn@users.noreply.github.com>
2023-03-01 17:23:37 +0000

    fixed everything

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require("fs")
+let original_raw = fs.readFileSync("chips/originalchips.json");
+
+let original = JSON.parse(original_raw)
+
+var Chips = Object.values(original)[0]
+var UUIDS = Object.keys(Chips)
+var Keys = Object.values(Chips)
+var Out = {}
+
+var key = Keys[0]
+console.log(key["ReadonlyChipName"])
+
+for(var k of Keys){
+    Out[k["ReadonlyChipName"]] = {
+        "Description": k["Description"],
+        "IsBeta": k["IsBetaChip"],
+        "IsTrollingRisk": k["IsTrollingRisk"],
+        "DeprecationStage": k["DeprecationStage"],
+        "NodeDescs": k["NodeDescs"]
+    }
+}
+fs.writeFileSync("chips/chips.json", JSON.stringify(Out, null, 4))
+/*
+{
+    "Chip Name": {
+        Chip data
+    }
+}
+*/
